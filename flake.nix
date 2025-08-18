fd26fe32f6a6eba12cbbf941798e344716226636
labi-le <65619560+labi-le@users.noreply.github.com>
2025-08-18 03:05:41 +0000

    chore: update flake.nix and flake.lock to v0.1.2

--- flake.nix
+++ flake.nix
@@ -8,18 +8,18 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     let
-      version = "0.1.1";
+      version = "0.1.2";
       pname = "deal";
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
 
       systemConfigs = {
         x86_64-linux = {
           url = "https://github.com/labi-le/deal/releases/download/v${version}/deal-x86_64-unknown-linux-gnu.tar.xz";
-          hash = "sha256-AnU497Kyo5XwO2KXfZ3ibzNwivcMoAVl/1UXEIr6zco="; # x86_64-linux
+          hash = "sha256-G/b9vCpKtoKQK9Wi5CfwRZpVKDXYuwovIi7zKOMQScU="; # x86_64-linux
         };
         aarch64-linux = {
           url = "https://github.com/labi-le/deal/releases/download/v${version}/deal-aarch64-unknown-linux-gnu.tar.xz";
-          hash = "sha256-zdopsNTB/BLPNo7nB82bA68Q2MopOdLQuqv1kfLcMKM="; # aarch64-linux
+          hash = "sha256-9dmR3BAy3s33Xcn2s7YFkCCHwmIoe2Yj6iPkNKAnzYo="; # aarch64-linux
         };
       };
     in
